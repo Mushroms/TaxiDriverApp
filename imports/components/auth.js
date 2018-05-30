483051b61c73d80927cadc0f7896d3f7c5e0fe52
mihon <mihon@zencoders.ru>
2018-05-30 20:53:41 +0300

    Make state work with axios and it's async nature

--- imports/components/auth.js
+++ imports/components/auth.js
@@ -8,15 +8,19 @@ import {
 import TextInputWithLabel from './ui/TextInputWithLabel';
 import Sockets from 'react-native-sockets';
 import { StackNavigator } from 'react-navigation';
+import axios from 'axios';
+
 
 
 
 export default class Auth extends Component {
   constructor(props) {
     super(props)
-    this.state = { allDrivers: {} }
+    this.state = {
+      login: '',
+      password: '',
+    }
   }
-
 
 
   static navigationOptions = {
@@ -46,71 +50,39 @@ export default class Auth extends Component {
 
 
   handleFormSubmit(navigate) {
-    let allDrivers = {};
-    let currentDriverId = 1;
-    let axios = require('axios')
-    let config = {
+    const config = {
       headers: {'x-api-key': 'xXxsupersecretapikeyxXx'}
     };
     const data = {
-       "username": "driver",
-       "password": "1234",
+      "username": "driver",
+      "password": "1234",
     };
 
-    let params = {
-      ID: ''
-    };
     axios.post('https://madcatz.org:3005/api/driver/auth', data, config)
       .then(function(response) {
         if (response.data.success === 1) {
           console.log('we got a success message boyz!');
           navigate('Queue',
             {
-               currentDriverId: response.data.uid,
-               allDrivers: response,
-             }
+              currentDriverId: response.data.uid,
+            }
           );
-        } else {
-          console.log('we are not in dah IF statement bitches');
         }
       })
       .catch(function (error) {
         console.log('Here we go and error: ', error);
       });
-
-    // axios.get('https://madcatz.org:3005/api/drivers/getall', config, params)
-    // .then(function(response){
-    //   console.log(response);
-    // })
-    // .catch(function(error){
-    //   console.log(error);
-    //
-    // });
-    
-
    }
-
-    //}
-
-
-    //let allDrivers = {};
-
-    //This id should be in response on success auth request
-    //let currentDriverId = 1;
-
-
 
 
   renderAuthForm(styles, navigate) {
     return (
       <View style={styles.formContainer}>
-        <TextInputWithLabel
-          labelText='Логин:'
-        >
+        <TextInputWithLabel labelText='Логин:'>
           <TextInput
             style={styles.textInput}
             onChangeText={(name) => this.setState({name})}
-            value={this.state.text}
+            value={this.state.login}
           />
         </TextInputWithLabel>
 
@@ -120,7 +92,7 @@ export default class Auth extends Component {
           <TextInput
             style={styles.textInput}
             onChangeText={(surname) => this.setState({surname})}
-            value={this.state.text}
+            value={this.state.password}
             secureTextEntry={true}
           />
         </TextInputWithLabel>
@@ -140,9 +112,7 @@ export default class Auth extends Component {
 
     return (
       <View>
-        <Text
-          style={styles.mainHeader}
-        >
+        <Text style={styles.mainHeader}>
           Авторизация
         </Text>
 
